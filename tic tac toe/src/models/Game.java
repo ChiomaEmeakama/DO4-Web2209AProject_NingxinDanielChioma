90f605ea0d4534cc725e93f15c4be72dff982ca9
ChiomaEm <120136376+ChiomaEmeakama@users.noreply.github.com>
2022-12-28 05:10:54 -0500

    PlayerView 1

--- tic tac toe/src/models/Game.java
+++ tic tac toe/src/models/Game.java
@@ -1,15 +1,18 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Game {
     private final Square[][] squares;
     private final ArrayList<IGameListener> listeners;
     private Square turn;
+    private boolean firstTurn;
+    private Random firstToPlay;
     public Game() {
         squares = new Square[3][3];
         listeners = new ArrayList<>();
-
+        firstToPlay = new Random(); //input first turn's code TODO
         turn=Square.X;
 
         // TODO: Initialize squares to be empty
