9c2fe77cf4e8b7bc4255490126a7648c4bb1679f
chaudchat <ningxin.zheng1227@gmail.com>
2022-12-28 14:26:17 -0500

    Merge branch 'main' of https://github.com/ChiomaEmeakama/DO4-Web2209AProject_NingxinDanielChioma

--- tic tac toe/src/controllers/GameController.java
+++ tic tac toe/src/controllers/GameController.java
@@ -32,39 +32,168 @@ public class GameController {
     private void on0BottomClicked(ActionEvent event)
     {
         game.setSquares(0);
+        if(!game.checkEmpty(0))
+            view.disableButton(0);
+        if(game.checkWinnerO()){
+            view.displayWinner(game.getPlayerOName());
+
+        }else if(game.checkWinnerX()){
+            //todo
+            view.displayWinner(game.getPlayerOName());
+
+        }else if(game.canDraw())
+            view.displayDraw();
+
+        window.resizeToFitContent();
+
+
 
     }
     private void on1BottomClicked(ActionEvent event)
     {
         game.setSquares(1);
 
+        if(!game.checkEmpty(1))
+            view.disableButton(1);
+        if(game.checkWinnerO()){
+            view.displayWinner(game.getPlayerOName());
+
+        }else if(game.checkWinnerX()){
+            //todo
+            view.displayWinner(game.getPlayerOName());
+
+        }else if(game.canDraw())
+            view.displayDraw();
+
+
+        window.resizeToFitContent();
+
     }private void on2BottomClicked(ActionEvent event)
     {
         game.setSquares(2);
 
+        if(!game.checkEmpty(2))
+            view.disableButton(2);
+        if(game.checkWinnerO()){
+            view.displayWinner(game.getPlayerOName());
+
+        }else if(game.checkWinnerX()){
+            //todo
+            view.displayWinner(game.getPlayerOName());
+
+        }else if(game.canDraw())
+            view.displayDraw();
+
+
+        window.resizeToFitContent();
     }private void on3BottomClicked(ActionEvent event)
     {game.setSquares(3);
 
+
+        if(!game.checkEmpty(3))
+            view.disableButton(3);
+        if(game.checkWinnerO()){
+            view.displayWinner(game.getPlayerOName());
+
+        }else if(game.checkWinnerX()){
+            //todo
+            view.displayWinner(game.getPlayerOName());
+
+        }else if(game.canDraw())
+            view.displayDraw();
+
+
+        window.resizeToFitContent();
 
     }private void on4BottomClicked(ActionEvent event)
     {
         game.setSquares(4);
 
+        if(!game.checkEmpty(4))
+            view.disableButton(4);
+        if(game.checkWinnerO()){
+            view.displayWinner(game.getPlayerOName());
+
+        }else if(game.checkWinnerX()){
+            //todo
+            view.displayWinner(game.getPlayerOName());
+
+        }else if(game.canDraw())
+            view.displayDraw();
+
+
+        window.resizeToFitContent();
     }private void on5BottomClicked(ActionEvent event)
     {
 
         game.setSquares(5);
+
+        if(!game.checkEmpty(5))
+            view.disableButton(5);if(game.checkWinnerO()){
+        view.displayWinner(game.getPlayerOName());
+
+    }else if(game.checkWinnerX()){
+        //todo
+        view.displayWinner(game.getPlayerOName());
+
+    }else if(game.canDraw())
+        view.displayDraw();
+
+
+        window.resizeToFitContent();
     }private void on6BottomClicked(ActionEvent event)
     {
 
         game.setSquares(6);
+
+        if(!game.checkEmpty(6))
+            view.disableButton(6);if(game.checkWinnerO()){
+        view.displayWinner(game.getPlayerOName());
+
+    }else if(game.checkWinnerX()){
+        //todo
+        view.displayWinner(game.getPlayerOName());
+
+    }else if(game.canDraw())
+        view.displayDraw();
+
+
+        window.resizeToFitContent();
     }private void on7BottomClicked(ActionEvent event)
     {
         game.setSquares(7);
 
+        if(!game.checkEmpty(7))
+            view.disableButton(7);if(game.checkWinnerO()){
+        view.displayWinner(game.getPlayerOName());
+
+    }else if(game.checkWinnerX()){
+        //todo
+        view.displayWinner(game.getPlayerOName());
+
+    }else if(game.canDraw())
+        view.displayDraw();
+
+
+        window.resizeToFitContent();
+
     }private void on8BottomClicked(ActionEvent event)
     {
         game.setSquares(8);
+
+        if(!game.checkEmpty(8))
+            view.disableButton(8);if(game.checkWinnerO()){
+        view.displayWinner(game.getPlayerOName());
+
+    }else if(game.checkWinnerX()){
+        //todo
+        view.displayWinner(game.getPlayerOName());
+
+    }else if(game.canDraw())
+        view.displayDraw();
+
+
+        window.resizeToFitContent();
 
     }
 
--- tic tac toe/src/views/GameView.java
+++ tic tac toe/src/views/GameView.java
@@ -3,36 +3,47 @@ package views;
 import models.Game;
 import models.Square;
 import utility.swing.layout.LayoutHelper;
+import utility.swing.windows.Window;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class GameView extends JPanel {
-    private final JButton[][] gameField;
+    private final JButton[][] gameField; //grid
 public GameView(Game game) {
         gameField = new JButton[3][3];
+       // JPanel panel = new JPanel();
+ /*   private boolean firstTurn;
+  //  private Random firstToPlay;
+    public Game() {
+        squares = new Square[3][3];
+        listeners = new ArrayList<>();
+     //   firstToPlay = new Random(); //input first turn's code TODO
+        turn=Square.X;
+*/
 
-        JPanel panel = new JPanel();
+
+        gameField[0][0]=new JButton();
+        gameField[0][1]=new JButton();
+        gameField[0][2]=new JButton();
+        gameField[1][0]=new JButton();
+        gameField[1][1]=new JButton();
+        gameField[1][2]=new JButton();
+        gameField[2][0]=new JButton();
+        gameField[2][1]=new JButton();
+        gameField[2][2]=new JButton();
 
         setLayout(new GridLayout(3,3));
-    System.out.println("here");
-        gameField[0][0]=new JButton(" ");
-        gameField[0][1]=new JButton(" ");
-        gameField[0][2]=new JButton(" ");
-        gameField[1][0]=new JButton(" ");
-        gameField[1][1]=new JButton(" ");
-        gameField[1][2]=new JButton(" ");
-        gameField[2][0]=new JButton(" ");
-        gameField[2][1]=new JButton(" ");
-        gameField[2][2]=new JButton(" ");
+        setBorder(LayoutHelper.createLargeEmptyBorder());
 
     for(JButton[] fields : gameField){
         for(JButton field: fields) {
-            panel.add(field);
+           add(field);
 
         }
     }
+
 /*
 
     for(JButton[] fields : gameField){
@@ -45,9 +56,52 @@ public GameView(Game game) {
 */
 
     }
+
+    public JPanel displayWinner(String name){
+    JPanel winnerPanel=new JPanel();
+    winnerPanel.setLayout(new BoxLayout(winnerPanel, BoxLayout.X_AXIS));
+    JLabel label=new JLabel("winner is : "+name);
+    winnerPanel.add(label);
+    return winnerPanel;
+}
+public JPanel displayDraw(){
+   JPanel drawPanel = new JPanel();
+    drawPanel.setLayout(new BoxLayout(drawPanel, BoxLayout.X_AXIS));
+    JLabel label=new JLabel("we have no winner / draw");
+    drawPanel.add(label);
+    return drawPanel;
+}
+
+    public void disableButton(int position){
+        switch (position) {
+            case 0:
+                gameField[0][0].setEnabled(false) ;
+
+            case 1:
+                gameField[0][1].setEnabled(false);
+            case 2:
+                gameField[0][2].setEnabled(false);
+            case 3:
+                gameField[1][0].setEnabled(false);
+            case 4:
+                gameField[1][1].setEnabled(false);
+            case 5:
+                gameField[1][2].setEnabled(false);
+            case 6:
+                gameField[2][0].setEnabled(false);
+            case 7:
+                gameField[2][1].setEnabled(false);
+            case 8:
+                gameField[2][2].setEnabled(false);
+
+
+        }
+    }
+
     public void add0BottomClicked(ActionListener listener)
     {
         gameField[0][0].addActionListener(listener);
+
     }
     public void add1BottomClicked(ActionListener listener)
     {
